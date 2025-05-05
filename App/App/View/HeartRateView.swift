ced091d0ba941db5f20df7a10586519c85f8094e
Muhammad Abid <muhammmadabidd@gmail.com>
2025-05-05 12:36:51 +0700

    Added a Segment Control

--- App/App/View/HeartRateView.swift
+++ App/App/View/HeartRateView.swift
@@ -7,10 +7,44 @@
 
 import SwiftUI
 
+struct SegmentControl: View {
+    @State private var selected = "HR"
+    let options = ["HR", "RHR", "HRV"]
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(options, id: \.self) { option in
+                Text(option)
+                    .fontWeight(.medium)
+                    .foregroundColor(selected == option ? .white : .white.opacity(0.6))
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(selected == option ? Color.red : Color.gray)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .onTapGesture {
+                        selected = option
+                    }
+            }
+        }
+        .padding(4)
+        .background(Color.gray.opacity(0.5))
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+    }
+}
+
+
 public struct HeartRateView: View {
     public var body: some View {
-        Text("Hello, World!")
-        .padding()
+        
+        VStack() {
+            Text("Heart Rate")
+            .padding()
+            
+            SegmentControl()
+            Spacer()
+        }
+        
+        
     }
 }
 
