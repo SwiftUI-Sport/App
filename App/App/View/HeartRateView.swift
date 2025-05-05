dd243331e6777f743a3f9d3c4f876ecd6160ecb1
Muhammad Abid <muhammmadabidd@gmail.com>
2025-05-05 12:25:48 +0700

    Crreated File

---
+++ App/App/View/HeartRateView.swift
@@ -0,0 +1,19 @@
+//
+//  HeartRateView.swift
+//  App
+//
+//  Created by Muhammad Abid on 05/05/25.
+//
+
+import SwiftUI
+
+public struct HeartRateView: View {
+    public var body: some View {
+        Text("Hello, World!")
+        .padding()
+    }
+}
+
+#Preview {
+    HeartRateView()
+}
