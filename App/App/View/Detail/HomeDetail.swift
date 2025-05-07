ffb916db08e2e34992f94c4dea93151bdafc7d22
Ali An Nuur <alianur043@gmail.com>
2025-05-07 11:26:44 +0700

    Add detail

--- App/App/View/Detail/HomeDetail.swift
+++ App/App/View/Detail/HomeDetail.swift
@@ -50,3 +50,4 @@ struct ThirdScreenView: View {
         .navigationBarTitleDisplayMode(.large)
     }
 }
+//dada
--- App/App/View/HeartRateView.swift
+++ App/App/View/HeartRateView.swift
@@ -24,6 +24,7 @@ struct SegmentControl: View {
                     .onTapGesture {
                         selected = option
                     }
+                //da
             }
         }
         .padding(4)
